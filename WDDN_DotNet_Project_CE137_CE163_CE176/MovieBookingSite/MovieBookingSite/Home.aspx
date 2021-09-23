70ec954b1155baa9321e41dc62879e68689f0f1a
dhshah1112 <dhshah1112@gmail.com>
2021-09-23 16:28:27 +0530

    Retrieve tickets for all movies

---
+++ WDDN_DotNet_Project_CE137_CE163_CE176/MovieBookingSite/MovieBookingSite/Home.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="MovieBookingSite.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--<asp:Button ID="Button1" runat="server" Text="Show all tickets available" onClick = "Show" />--%>
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/ShowTickets.aspx">ShowTickets</asp:HyperLink>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WDDN_DotNet_Project_CE137_CE163_CE176/MovieBookingSite/MovieBookingSite/ShowTickets.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+namespace MovieBookingSite
+{
+    public partial class ShowTickets : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = WebConfigurationManager.ConnectionStrings["ConStr"].ConnectionString;
+            try
+            {
+                using (con) {
+                    con.Open();
+                    String retrieveMovies = "select * from Ticket";
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(retrieveMovies, con);
+                    da.Fill(dt);
+                    foreach(DataRow row in dt.Rows)
+                    {
+                        Response.Write(row["MovieName"].ToString());
+                        
+                        
+                    }
+                    con.Close();
+
+                }
+            }
+            catch(Exception err)
+            {
+                Response.Write(err.Message);
+            }
+        }
+    }
+}
